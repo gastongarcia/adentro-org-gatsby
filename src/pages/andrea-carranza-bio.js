e4084b932121578d2bde759f0cf885f365484112
gastongarcia <gaston.garcia@gmail.com>
2019-08-01 11:21:16 -0600

    Foto de Andy en ingles

--- src/pages/andrea-carranza-bio.js
+++ src/pages/andrea-carranza-bio.js
@@ -30,6 +30,21 @@ const AndyPage = ({ data }) => {
       <Header PageTitle={data.datoCmsProfileAndy.tituloDeLaPagina} />
 
       <section>
+
+        <div className="sm:flex">
+          <div className="profile-pic">
+            <img src={data.datoCmsProfileAndy.picture.fluid.src} alt={data.datoCmsProfileAndy.tituloDeLaPagina}
+            className="p-3 border border-grey-light mt-3" />
+          </div>
+
+          <div
+            class="max-w-xl md:max-w-3xl lg:max-w-4xl mx-auto"
+            dangerouslySetInnerHTML={{
+            __html: data.datoCmsProfileAndy.bio
+            }}
+          />
+        </div>
+
         <div
         class="max-w-xl md:max-w-3xl lg:max-w-4xl mx-auto"
         dangerouslySetInnerHTML={{
