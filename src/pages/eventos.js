41dad2689a9080e6b1a958e899f4986ec96d6013
Gastón García <gaston.garcia@gmail.com>
2020-01-10 15:21:29 -0600

    agregando logos

--- src/pages/eventos.js
+++ src/pages/eventos.js
@@ -6,6 +6,8 @@ import Header from "../components/header";
 import styled from "styled-components";
 import Navbar from "../components/navbar";
 import Lotus from "../components/lotus";
+import Mexico from "../images/mexico.png";
+import Asc from "../images/asc.gif";
 
 const StyledLink = styled(Link)`
   color: #4a5568;
@@ -74,6 +76,23 @@ const Eventos = ({ data }) => {
       </section>
 
       <Lotus />
+
+      <section>
+        <div className="max-w-xl md:max-w-3xl lg:max-w-4xl mx-auto">
+          <p className="text-sm">En conjunto con:</p>
+          <img
+            src={Mexico}
+            alt="Instituto Mexicano de Mindfulness"
+            style={{ maxWidth: "200px", border: "3px solid white" }}
+          />
+          <p className="text-sm">A beneficio de:</p>
+          <img
+            src={Asc}
+            alt="Asociación Sueña en Colores"
+            style={{ maxWidth: "200px", border: "3px solid white" }}
+          />
+        </div>
+      </section>
     </Layout>
   );
 };
