a1dba3ce0646b1037acbc3a65693f05c99426764
Gastón García <gaston.garcia@gmail.com>
2020-01-30 18:44:57 -0600

    Agregando Meta Tag de Google Gsuite

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { StaticQuery, graphql } from "gatsby";
+import Helmet from "react-helmet";
 import "./layout.css";
 import "../css/style.css";
 import Footer from "./footer";
@@ -19,6 +20,12 @@ function Layout({ children }) {
       `}
       render={data => (
         <>
+          <Helmet>
+            <meta
+              name="google-site-verification"
+              content="RWebUIX0OsNyAbiMrIkz5gSMvFU2kxhOGiEjn1oRzcs"
+            />
+          </Helmet>
           <div className="flex flex-col flex-1 md:justify-center w-full md:w-3/5 mx-auto px-4 py-8 md:p-8">
             {children}
           </div>
